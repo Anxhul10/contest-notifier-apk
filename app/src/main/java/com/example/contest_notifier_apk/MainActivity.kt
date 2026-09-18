56cab3deb02eda0b7b720dd63398256482c04553
Anshul Ekka <anxhul10@gmail.com>
2026-09-18 18:26:23 +0530

    feat: enable dark mode and light mode according to user system setting

--- app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
+++ app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
@@ -5,18 +5,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.contest_notifier_apk.ui.theme.ContestnotifierapkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ContestNotifierScreen()
+            ContestnotifierapkTheme {
+                ContestNotifierScreen()
+            }
         }
     }
 }
 @Composable
 fun ContestNotifierScreen() {
-    Text("hello world")
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Text("hello world")
+    }
 }
