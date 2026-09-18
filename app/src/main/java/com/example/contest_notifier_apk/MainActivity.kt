d439f1a98b9e2b167784b3862d3c996e92ad996e
Anshul Ekka <anxhul10@gmail.com>
2026-09-18 19:26:04 +0530

    temp: inc cntr on click

--- app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
+++ app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
@@ -5,10 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.contest_notifier_apk.ui.theme.ContestnotifierapkTheme
 
@@ -25,10 +31,28 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun ContestNotifierScreen() {
+    val numbers = listOf(1,2,3)
+
+    var currentIdx by remember {
+        mutableStateOf(0)
+    }
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Text("hello world")
+
+        Column {
+            Text(
+                text = numbers[currentIdx].toString()
+            )
+            Button(
+                onClick = {
+                    currentIdx = (currentIdx + 1) % numbers.size
+                }
+            ) {
+                Text(text = "click me")
+            }
+        }
     }
 }
