0e22b96ad50316baf782272b64475c59c5b18ccb
Anshul Ekka <anxhul10@gmail.com>
2026-09-18 18:28:47 +0530

    chore: disable  edge to edge

--- app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
+++ app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
@@ -15,7 +15,7 @@ import com.example.contest_notifier_apk.ui.theme.ContestnotifierapkTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             ContestnotifierapkTheme {
                 ContestNotifierScreen()
