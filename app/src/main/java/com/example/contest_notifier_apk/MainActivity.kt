48a7951aaee5b0150775a510d0ee68dcd7c2a9a5
Anshul Ekka <anxhul10@gmail.com>
2026-09-25 17:12:09 +0530

    refactor: seperate fontfamily from main activity

--- app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
+++ app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
@@ -52,6 +52,7 @@ import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.googlefonts.Font
+import com.example.contest_notifier_apk.utils.fontFamily
 
 class MainActivity : ComponentActivity() {
 
@@ -65,22 +66,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-val provider = GoogleFont.Provider(
-    providerAuthority = "com.google.android.gms.fonts",
-    providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs
-)
-
-val fontName = GoogleFont("Lobster Two")
-
-val fontFamily = FontFamily(
-    Font(
-        googleFont = fontName,
-        fontProvider = provider,
-        weight = FontWeight.Thin,
-        style = FontStyle.Italic
-    )
-)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
---
+++ app/src/main/java/com/example/contest_notifier_apk/utils/fontFamily.kt
@@ -0,0 +1,25 @@
+package com.example.contest_notifier_apk.utils
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.example.contest_notifier_apk.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Lobster Two")
+
+val fontFamily = FontFamily(
+    Font(
+        googleFont = fontName,
+        fontProvider = provider,
+        weight = FontWeight.Thin,
+        style = FontStyle.Italic
+    )
+)
