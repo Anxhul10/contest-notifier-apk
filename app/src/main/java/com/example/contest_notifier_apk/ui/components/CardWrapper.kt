a9770843c1f014fb2c8333c061c412b04cf2fda2
Anshul Ekka <anxhul10@gmail.com>
2026-09-27 00:20:30 +0530

    chore: pass new argument

--- app/src/main/java/com/example/contest_notifier_apk/ui/components/CardWrapper.kt
+++ app/src/main/java/com/example/contest_notifier_apk/ui/components/CardWrapper.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Modifier
 @Composable
 fun CardWrapper(
     modifier: Modifier = Modifier,
-    names: List<String> = List(10) {"$it"}
+    names: List<String>
 ) {
     LazyColumn() {
         items(items = names) {
