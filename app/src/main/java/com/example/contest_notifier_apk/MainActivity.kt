2edf6e85fc5a31c0a5a3367e517f91230fb7649d
Anshul Ekka <anxhul10@gmail.com>
2026-09-27 00:21:40 +0530

    refactor: my app as contest list & pass new argument on cardwrapper

--- app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
+++ app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
@@ -1,38 +1,17 @@
 package com.example.contest_notifier_apk
 
-import android.content.Intent
 import android.content.res.Configuration
-import android.net.Uri
-import androidx.compose.ui.platform.LocalContext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.contest_notifier_apk.ui.components.Card
 import com.example.contest_notifier_apk.ui.components.CardWrapper
 import com.example.contest_notifier_apk.ui.theme.ContestnotifierapkTheme
-import com.example.contest_notifier_apk.ui.theme.Purple40
-import com.example.contest_notifier_apk.utils.fontFamily
 import com.example.contest_notifier_apk.ui.components.TopBar
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,17 +24,14 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
-fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("user 1", "user 2", "user 3")) {
+fun ContestList(modifier: Modifier = Modifier, names: List<String> = listOf("contest 1", "contest 2", "contest 3", "contest 4", "contest 5", "contest 6", "contest 7", "contest 8", "contest 9")) {
     Surface (
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
         Column () {
-            for(name in names) {
-                CardWrapper(modifier)
-            }
+            CardWrapper(modifier, names)
         }
     }
 }
