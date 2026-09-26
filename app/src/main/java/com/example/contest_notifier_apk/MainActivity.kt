d0f5f49c189e959880ca197d52e6dcd290204638
Anshul Ekka <anxhul10@gmail.com>
2026-09-26 23:44:29 +0530

    chore: clean unused imports

--- app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
+++ app/src/main/java/com/example/contest_notifier_apk/MainActivity.kt
@@ -2,28 +2,18 @@ package com.example.contest_notifier_apk
 
 import android.content.Intent
 import android.content.res.Configuration
-import android.graphics.pdf.content.PdfPageGotoLinkContent
 import android.net.Uri
 import androidx.compose.ui.platform.LocalContext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -35,23 +25,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.contest_notifier_apk.ui.theme.ContestnotifierapkTheme
-import com.example.contest_notifier_apk.ui.theme.Pink40
 import com.example.contest_notifier_apk.ui.theme.Purple40
-import kotlinx.coroutines.delay
-import androidx.compose.ui.text.googlefonts.GoogleFont
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.googlefonts.Font
 import com.example.contest_notifier_apk.utils.fontFamily
 
 class MainActivity : ComponentActivity() {
